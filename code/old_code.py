943e9580e0d2b274cc2fb7b82d3e09b9b2e242c2
Reece Yang <reeceyang@live.com>
2023-03-21 22:35:23 -0400

    transcribe function snippets

--- code/old_code.py
+++ code/old_code.py
@@ -1,3 +1,6 @@
+import os
+from xml import etree as et
+
 NAME_TO_MSTRKS = [
   ({"Rast", "Evc", "Rehavi", "Pençgâh", "Sazkar", "Sâzkâr", "Nevâ", "Neva Büselik", "Tahir", "Tâhir Büselik", "Hüseyni", "Hüseyni ‘Asiran", "Gülizâr", "Yegâh", "Beyâti Araban", "Muhayyer", "Muhayyer Büselik", "Gerdâniye",  "Gerdaniye Büselik", "Dilkeş Haveran", "Evc Büselik", "Irak"}, ["B -0.5", "F 1"]), 
   ({"Acem Kürdi", "Acem Asiran", "Kürdi", "Muhayyer-Kiird"}, ["B -1"]), 
@@ -38,3 +41,47 @@ def strks_to_xml(strks):
   xmlstr += "<key-accidental>" + key_accidental + "</key-accidental>"
 
   return xmlstr #return the full XML.
+
+
+for folder in os.listdir(filepath): #"." refers to current directory.
+  #now, we know the string of folder, and can access all the files within!
+  if folder[0] != ".": #see tech note below
+    for musicfile in os.listdir(filepath + "/" + folder): #like "./CT 1-5
+      """
+      Quick tech note: "." indicates a file is HIDDEN.
+      We don't wish to access hidden files, like .DS_Store and .mscbackup.
+      Therefore, if a file starts with ., we are guaranteed that it's hidden.
+      Of course, we don't have pathological file names that intentionally
+      start with ".", so we assume start with "." -> don't consider it.
+      """
+      if musicfile[0] != "." and ".musicxml" in musicfile: #ignore if otherwise
+        #now we are faced with JUST files. Now modify!
+
+        #figure out which name is inside
+        for name in ALL_NAMES:
+        #at least one must be true
+          if name in musicfile:
+            mstrks_cor = mstrks(name) #find the correct list of key sigs to modify
+
+          #at this point mstrks must exist.
+          insertks(
+            filepath + "/" + folder + "/" + musicfile, 
+            mstrks_to_xml(mstrks_cor)
+          ) #gives us correct modification
+
+#start with xml
+print("FILE PATH:", filepath)
+tree = et.parse(filepath)
+root = tree.getroot()
+print("ROOT:", root)
+
+"""
+this is where we need to hard code this number.
+Within Musescore, our root is just the score-partwise.
+Now with that being said, the 6th child (index 5) is called part id="P1".
+Then, within that, the 1st child (index 0) is callsed measure number="1" etc
+And the second child (index 1) is called attributes
+finally, the second child within that (index 1) is called key.
+"""
+desiredroot = root[5][0][1][1] #this is where the part of the key is stored
+print("DESIRED ROOT:", desiredroot)
